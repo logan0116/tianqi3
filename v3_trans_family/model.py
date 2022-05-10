7fe0c6269fde81a2399e067343db85f6bf9a013c
logan0116 <1809361150@qq.com>
2022-05-10 16:42:17 +0800

    logan-0510-update

--- v3_trans_family/model.py
+++ v3_trans_family/model.py
@@ -136,7 +136,7 @@ class TransR(nn.Module):
         self.dim = dim
         self.node_emb = self._init_node_emb()
         self.link_emb = self._init_link_emb()
-        self.transfer_matrix = nn.Embedding(self.link_size, self.dim * self.dim)
+        self.transfer_matrix = self._init_transfer_matrix()
         self.criterion = nn.MarginRankingLoss(margin=margin, reduction='none')
 
     def _init_node_emb(self):
@@ -152,6 +152,13 @@ class TransR(nn.Module):
         uniform_range = 6 / np.sqrt(self.dim)
         link_emb.weight.data.uniform_(-uniform_range, uniform_range)
         return link_emb
+
+    def _init_transfer_matrix(self):
+        transfer_matrix = nn.Embedding(num_embeddings=self.link_size,
+                                       embedding_dim=self.dim * self.dim)
+        uniform_range = 6 / self.dim
+        transfer_matrix.weight.data.uniform_(-uniform_range, uniform_range)
+        return transfer_matrix
 
     def forward(self, sp, tp, sn, tn, r):
         positive_distances = self._distance(sp, r, tp)
@@ -177,68 +184,67 @@ class TransR(nn.Module):
         r_emb = self.link_emb(r)
         # 超平面的法向量
         r_transfer = self.transfer_matrix(r)
-        h_trans = self._transfer(s_emb, r_transfer)
+        s_trans = self._transfer(s_emb, r_transfer)
         t_trans = self._transfer(t_emb, r_transfer)
 
-        return (h_trans + r_emb - t_trans).norm(p=self.norm, dim=1)
+        return (s_trans + r_emb - t_trans).norm(p=self.norm, dim=1)
 
 
-class TransD(nn.Module):
-
-    def __init__(self, node_size, link_size, device, norm, dim, margin):
-        super(TransD, self).__init__()
-        self.node_size = node_size
-        self.link_size = link_size
-        self.device = device
-        self.norm = norm
-        self.dim = dim
-        self.node_emb = self._init_node_emb()
-        self.link_emb = self._init_link_emb()
-        self.transfer_matrix = nn.Embedding(self.link_size, self.dim * self.dim)
-        self.criterion = nn.MarginRankingLoss(margin=margin, reduction='none')
-
-    def _init_node_emb(self):
-        node_emb = nn.Embedding(num_embeddings=self.node_size,
-                                embedding_dim=self.dim)
-        uniform_range = 6 / np.sqrt(self.dim)
-        node_emb.weight.data.uniform_(-uniform_range, uniform_range)
-        return node_emb
-
-    def _init_link_emb(self):
-        link_emb = nn.Embedding(num_embeddings=self.link_size,
-                                embedding_dim=self.dim)
-        uniform_range = 6 / np.sqrt(self.dim)
-        link_emb.weight.data.uniform_(-uniform_range, uniform_range)
-        return link_emb
-
-    def forward(self, sp, tp, sn, tn, r):
-        positive_distances = self._distance(sp, r, tp)
-        negative_distances = self._distance(sn, r, tn)
-        return self.loss(positive_distances, negative_distances)
-
-    def loss(self, positive_distances, negative_distances):
-        target = torch.tensor([-1], dtype=torch.long, device=self.device)
-        return self.criterion(positive_distances, negative_distances, target)
-
-    def _transfer(self, e, r_transfer):
-        r_transfer = r_transfer.view(-1, self.dim, self.dim)
-        e = e.view(-1, 1, self.dim)
-        e = torch.matmul(e, r_transfer)
-        return e.view(-1, self.dim)
-
-    def _distance(self, s, r, t):
-        """
-        Triplets should have shape Bx3 where dim 3 are head id, relation id, tail id.
-        """
-        s_emb = self.node_emb(s)
-        t_emb = self.node_emb(t)
-        r_emb = self.link_emb(r)
-        # 超平面的法向量
-        r_transfer = self.transfer_matrix(r)
-        h_trans = self._transfer(s_emb, r_transfer)
-        t_trans = self._transfer(t_emb, r_transfer)
-
-        return (h_trans + r_emb - t_trans).norm(p=self.norm, dim=1)
+# class TransD(nn.Module):
+#
+#     def __init__(self, node_size, link_size, device, norm, dim, margin):
+#         super(TransD, self).__init__()
+#         self.node_size = node_size
+#         self.link_size = link_size
+#         self.device = device
+#         self.norm = norm
+#         self.dim = dim
+#         self.node_emb = self._init_node_emb()
+#         self.link_emb = self._init_link_emb()
+#         self.transfer_matrix = nn.Embedding(self.link_size, self.dim * self.dim)
+#         self.criterion = nn.MarginRankingLoss(margin=margin, reduction='none')
+#
+#     def _init_node_emb(self):
+#         node_emb = nn.Embedding(num_embeddings=self.node_size,
+#                                 embedding_dim=self.dim)
+#         uniform_range = 6 / np.sqrt(self.dim)
+#         node_emb.weight.data.uniform_(-uniform_range, uniform_range)
+#         return node_emb
+#
+#     def _init_link_emb(self):
+#         link_emb = nn.Embedding(num_embeddings=self.link_size,
+#                                 embedding_dim=self.dim)
+#         uniform_range = 6 / np.sqrt(self.dim)
+#         link_emb.weight.data.uniform_(-uniform_range, uniform_range)
+#         return link_emb
+#
+#     def forward(self, sp, tp, sn, tn, r):
+#         positive_distances = self._distance(sp, r, tp)
+#         negative_distances = self._distance(sn, r, tn)
+#         return self.loss(positive_distances, negative_distances)
+#
+#     def loss(self, positive_distances, negative_distances):
+#         target = torch.tensor([-1], dtype=torch.long, device=self.device)
+#         return self.criterion(positive_distances, negative_distances, target)
+#
+#     def _transfer(self, e, r_transfer):
+#         r_transfer = r_transfer.view(-1, self.dim, self.dim)
+#         e = e.view(-1, 1, self.dim)
+#         e = torch.matmul(e, r_transfer)
+#         return e.view(-1, self.dim)
+#
+#     def _distance(self, s, r, t):
+#         """
+#         Triplets should have shape Bx3 where dim 3 are head id, relation id, tail id.
+#         """
+#         s_emb = self.node_emb(s)
+#         t_emb = self.node_emb(t)
+#         r_emb = self.link_emb(r)
+#         r_transfer = self.transfer_matrix(r)
+#         s_trans = self._transfer(s_emb, r_transfer)
+#         t_trans = self._transfer(t_emb, r_transfer)
+#
+#         return (s_trans + r_emb - t_trans).norm(p=self.norm, dim=1)
 
 
 class Evaluator:
